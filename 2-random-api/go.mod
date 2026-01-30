1b38ec57011d76c27dc38c2ccc79b8bbd459ad74
Vic Shóstak <vic@shostak.dev>
2026-01-30 11:19:52 +0300

    Реализовано ДЗ (2-random-api)

---
+++ 2-random-api/go.mod
@@ -0,0 +1,3 @@
+module purpleschool-go-homework/2-random-api
+
+go 1.25.6
---
+++ 2-random-api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprint(w, rand.Intn(6))
+	})
+
+	server := &http.Server{
+		Addr:    ":8091",
+		Handler: router,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
